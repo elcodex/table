381dc7cad9602e4b37905685d238141f9c3d935d
Elen <elcrses@ya.ru>
2021-03-31 19:17:45 +0300

    table example application with css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Table from "./components/Table/Table";
+
+import { generator } from "./helpers/tableGenerator";
+
+import './css/table.css';
+
+interface TableRow {
+    [name: string]: string | number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState<TableRow[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fieldsNames = ["Title", "Description", "Rating", "AARD"];
+
+    useEffect(() => {
+        async function getData(): Promise<void> {
+            setIsLoading(true);
+            
+            setData(await generator([
+                {name: "Title", type: "text", length: 20},
+                {name: "Description", type: "text", length: 200},
+                {name: "Rating", type: "number", length: 5}
+            ]));
+
+            setIsLoading(false);
+        }
+
+        getData();
+    }, []);
+
+    if (isLoading) return <div>Loading data...</div>
+
+    return (
+        <div className="App">
+            <Table 
+                data={data}
+                fieldsNames={fieldsNames}
+                maxPageRows={10}
+
+                textForDataLength="Records:"
+                textForFromToDataRows="Rows:"
+                textForPageNumber="Page:"
+                textForFilterLabel="Filter:"
+            />
+        </div>
+    );
 }
 
 export default App;
